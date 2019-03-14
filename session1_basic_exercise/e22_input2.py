e17786f01f08c3e9c98b020b58b30194cca62ad8
athens <athens314@hotmail.com>
2019-03-14 17:20:30 -0400

    added session1

--- session1_basic_exercise/e22_input2.py
+++ session1_basic_exercise/e22_input2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Assuming a text-based command line interactive login program
 # prompt to input your name
 # prompt to input your password (Do not use your real password, and you may input like 12345)
