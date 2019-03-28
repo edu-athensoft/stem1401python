e827b8f0821759d0c951e7fb984fc690ebe06753
athensedu <lada314@gmail.com>
2019-03-28 13:48:59 -0400

    added literal boolean

---
+++ session2_syntax/s13_literal_boolean.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# _*_ code:utf-8 _*_
+
+"""literals of boolean"""
+
+x = (1 == True)
+y = (1 == False)
+a = True + 4
+b = False + 10
+
+print("x is", x)
+print("y is", y)
+print("a:", a)
+print("b:", b)
