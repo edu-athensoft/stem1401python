b86c77c7de9680f9bdea2110d9f969895955c09f
athensedu <lada314@gmail.com>
2019-03-28 13:42:03 -0400

    added literal numeric, string

---
+++ session2_syntax/s02_var.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# _*_ code:utf-8 _*_
+
+"""assign values to multiple variables"""
+
+a,b,c = 1, 1.5, 'hello I am a string'
+
+print(a)
+print(b)
+print(c)
+
+
+
---
+++ session2_syntax/s03_var.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# _*_ code:utf-8 _*_
+
+"""assign same value to multiple variables"""
+
+x = y = z = "Montreal"
+
+print(x)
+print(y)
+print(z)
